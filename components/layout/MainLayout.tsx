c336c19b3de91f5de6e3a89e949a7f64fa734a1a
Marianne Rose Napaño <231487693+ifwian@users.noreply.github.com>
2026-09-20 14:59:06 +0800

    Refactor MainLayout by removing Footer
    Co-authored-by: knrsvlcs <knrsvlcs@github.com>

--- components/layout/MainLayout.tsx
+++ components/layout/MainLayout.tsx
@@ -2,7 +2,6 @@
 
 import { ReactNode } from "react";
 import Sidebar from "./Sidebar";
-import Footer from "./Footer";
 import ScrollTopButton from "./ScrollTopButton";
 import ChatWithIan from "../chat/ChatWithIan";
 
@@ -13,16 +12,16 @@ interface MainLayoutProps {
 export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex min-h-screen">
-      {/* Wrap Sidebar in a wrapper div instead of passing className directly */}
+      {/* Sidebar wrapper */}
       <div className="w-56 shrink-0">
         <Sidebar />
       </div>
 
-      <main id="main-content" className="flex-1 flex flex-col items-center px-6 py-12">
-        <div className="w-full flex-1 flex flex-col items-center">
+      {/* Main content wrapper */}
+      <main id="main-content" className="flex-1 px-6 py-12">  
+        <div className="w-full">
           {children}
         </div>
-        <Footer />
       </main>
 
       <ScrollTopButton />
